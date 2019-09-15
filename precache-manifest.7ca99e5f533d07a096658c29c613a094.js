4f118784f5ef65336fcaf214943e0580dbd17958
geraldnguyen <thehuy.nguyen@mastercard.com>
2019-09-15 22:58:35 +0800

    Updates

---
+++ precache-manifest.7ca99e5f533d07a096658c29c613a094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4d527f90f81e88aa748d6ff423728d",
+    "url": "/multiplication-tests/index.html"
+  },
+  {
+    "revision": "1ec23df8c6a994501f5b",
+    "url": "/multiplication-tests/static/css/main.d9415094.chunk.css"
+  },
+  {
+    "revision": "b82adc475589bab89f32",
+    "url": "/multiplication-tests/static/js/2.6e101b59.chunk.js"
+  },
+  {
+    "revision": "1ec23df8c6a994501f5b",
+    "url": "/multiplication-tests/static/js/main.1233be06.chunk.js"
+  },
+  {
+    "revision": "36bdb709479a41cff456",
+    "url": "/multiplication-tests/static/js/runtime~main.a002960a.js"
+  }
+]);
